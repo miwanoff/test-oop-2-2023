7c4f3bc28662d8e82c49949684ff90d4b8c70f9f
miwanoff <marina.iva.noffa@gmail.com>
2023-01-22 15:22:53 +0200

    Add $name

--- MyCat.php
+++ MyCat.php
@@ -3,15 +3,23 @@ include "Cat.php";
 
 class MyCat extends Cat
 {
+    protected $name;
+
+    public function __construct($name = "", $age = 1)
+    {
+        parent::__construct($age);
+        $this->name = $name;
+    }
+
     public function sleep()
     {
         echo "\nZzzzz...\n";
     }
 }
 
-$my_cat = new MyCat(5);
+$my_cat = new MyCat("kitty", 5);
 
 echo $my_cat->getAge();
 $my_cat->sleep();
 
-$my_cat->foo ( new MyCat (8) );
+$my_cat->foo(new MyCat("Sam", 8));
