43fa83cff63f3864f52381666a632eac351deafb
miwanoff <marina.iva.noffa@gmail.com>
2023-01-22 16:26:29 +0200

    Add instanceof Shape

--- test.php
+++ test.php
@@ -2,7 +2,6 @@
 
 include "autoload.php";
 
-
 $rect1 = new RectangleDrawer(1, 2, 3, 4);
 $rect2 = new RectangleDrawer(5, 6, 7, 8);
 $circle3 = new CircleDrawer(15, 16, 3);
@@ -10,5 +9,7 @@ $circle4 = new CircleDrawer(25, 26, 6);
 $arr = [$rect1, $rect2, $circle3, $circle4];
 
 foreach ($arr as $value) {
-    echo $value->draw();
+    if ($value instanceof Shape) {
+        echo $value->draw();
+    }
 }
